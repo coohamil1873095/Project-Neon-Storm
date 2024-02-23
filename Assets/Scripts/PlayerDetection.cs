3726a8f7fe1b3aa20302909f8ad4fe81a7c3d2c9
AniSindhu <aniruddhsindhu@gmail.com>
2024-02-22 22:39:37 -0800

    Added enemy detection
    in an adjustable cone

---
+++ Assets/Scripts/PlayerDetection.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PlayerDetection : MonoBehaviour
+{
+    public float fov;   //how far you want the cone to extend from player
+    [Range(0, 360)] public float fovAngle;      //size of cone (in degrees)
+    private Collider2D target;
+
+    private void OnDrawGizmos()
+    {
+        Handles.color = new Color(1, 0, 0, 0.3f);
+        Handles.DrawSolidArc(transform.position , transform.forward , Quaternion.AngleAxis(-fovAngle/2f , transform.forward)*transform.up , fovAngle , fov);
+        //Handles.DrawSolidDisc(transform.position , transform.forward , fov);
+    }
+
+    private void Update()
+    {
+        target = Physics2D.OverlapCircle(transform.position, fov);
+        if (target && target.tag == "Enemy")
+        {
+            float signedAngle = Vector3.Angle(
+            transform.up,
+            target.transform.position - transform.position);
+            if (Mathf.Abs(signedAngle) < fovAngle / 2)
+            {
+                Debug.Log("found an enemy!");
+                target.GetComponent<Enemy>().DamageEnemy(1);
+                //Debug.Log("found an enemy!");
+            }
+        }
+    }
+}
