f702b0516369e117548f18eede6dc9bf55d654ed
coohamil1873095 <coohamil@ucsc.edu>
2024-02-22 23:43:29 -0800

    game hud added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
-    public static bool isPlaying { get; private set; } 
+    public static bool isPlaying { get; private set; }
+    private Canvas gameHUD;
+    [SerializeField] private TMP_Text XPText;
     
     void Awake() 
     {
@@ -18,13 +21,31 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        gameHUD = GameObject.Find("Game HUD").GetComponent<Canvas>();
+        
         isPlaying = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) && !PauseMenu.isPaused)
+        {
+            PauseMenu.isPaused = true;
+            Time.timeScale = 0;
+            toggleGameHUD(false);
+            MenuManager.OpenMenu(Menu.PAUSE_MENU, null);
+        }
+    }
+
+    public void UpdateXPCounter(int newXPVal)
+    {
+        XPText.SetText("XP: " + newXPVal);
+    }
+
+    public void toggleGameHUD(bool activeStatus) 
+    {
+        gameHUD.enabled = activeStatus;
     }
 
     public void OnLevelUp()
@@ -32,5 +53,10 @@ public class GameManager : MonoBehaviour
         Debug.Log("Player Leveled Up");
 
         PlayerManager.Instance.ResetPlayerXP(5);
+    }
+
+    public void QuitGame()
+    {
+        isPlaying = false;
     }
 }
