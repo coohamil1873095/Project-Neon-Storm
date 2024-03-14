3a534c6e9f581d38963460af5e8c11e11abe6fa1
coohamil1873095 <coohamil@ucsc.edu>
2024-03-13 22:05:57 -0700

    primary fire fixed

--- Assets/Scripts/Player Scripts/PlayerDetection.cs
+++ Assets/Scripts/Player Scripts/PlayerDetection.cs
@@ -8,12 +8,11 @@ public class PlayerDetection : MonoBehaviour
     [SerializeField] public float playerWeaponDamage;
     [SerializeField] private float playerWeaponCooldown;
     [SerializeField] public GameObject lightningEffect;
-    [SerializeField] public GameObject lightningEffectEnd;
+    //[SerializeField] public GameObject lightningEffectEnd;
     public float fov;   // How far you want the cone to extend from the player
     [Range(0, 360)] public float fovAngle;      // Size of cone (in degrees)
-    private Collider2D target;
+    private Collider2D[] targets;
     private bool isLeftMouseButtonDown = false;
-    private bool dmgActive = false;
 
     private void OnDrawGizmos()
     {
@@ -24,7 +23,7 @@ public class PlayerDetection : MonoBehaviour
             mousePosition.y = 0.5f;
             Handles.color = new Color(1, 0, 0, 0.3f);
             Handles.DrawSolidArc(transform.position, transform.forward, Quaternion.AngleAxis(-fovAngle / 2f, transform.forward) * transform.up, fovAngle, fov);
-            lightningEffect.SetActive(true);
+            
         }
     }
 
@@ -36,34 +35,38 @@ public class PlayerDetection : MonoBehaviour
         }
         else if (Input.GetMouseButtonUp(0))  // Left mouse button is released
         {
-            lightningEffect.SetActive(false);
             isLeftMouseButtonDown = false;
         }
 
         if (isLeftMouseButtonDown)
         {
-            target = Physics2D.OverlapCircle(transform.position, fov);
-            if (target && target.tag == "Enemy")
+            
+            targets = Physics2D.OverlapCircleAll(transform.position, fov, LayerMask.GetMask("Enemy"));
+            
+            foreach (Collider2D collider in targets) 
             {
                 float signedAngle = Vector3.Angle(
-                    transform.up,
-                    target.transform.position - transform.position);
+                transform.up,
+                collider.transform.position - transform.position);
 
-
-                if (Mathf.Abs(signedAngle) < fovAngle / 2 && !dmgActive)
+                if (Mathf.Abs(signedAngle) < fovAngle / 2 && !collider.GetComponent<Enemy>().EnemyBeingDamaged())
                 {
-                    StartCoroutine(ApplyDamage());
-                    lightningEffectEnd.transform.position = target.transform.position;
+                    StartCoroutine(ApplyDamage(collider));
+                    
+                    GameObject lightning = Instantiate(lightningEffect, GameObject.Find("Lightning").transform);
+                    Destroy(lightning, 0.25f);
+                    lightning.transform.Find("LightningStart").transform.position = transform.position;
+                    lightning.transform.Find("LightningEnd").transform.position = collider.transform.position;
                 }
             }
+
+            
         }
     }
 
-    IEnumerator ApplyDamage()
+    IEnumerator ApplyDamage(Collider2D col)
     {
-        target.GetComponent<Enemy>().DamageEnemy(playerWeaponDamage);
-        dmgActive = true;
-        yield return new WaitForSeconds(playerWeaponCooldown);
-        dmgActive = false;
+        col.GetComponent<Enemy>().DamageEnemy(playerWeaponDamage);
+        yield return null;
     }
 }
